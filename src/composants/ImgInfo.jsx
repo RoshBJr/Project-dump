14c2ab18a03cd8ac7ce42bee43f4bddbe545785f
RoshBJr <roshanjr3@gmail.com>
2023-12-20 16:33:09 -0500

    modified hover effects on day changing buttons, modified listing scrollbar appearance

--- src/composants/ImgInfo.jsx
+++ src/composants/ImgInfo.jsx
@@ -3,6 +3,8 @@ import dateAjd from '../code/dateAjd';
 import './ImgInfo.scss';
 import { IdjCtx} from '../Appli';
 import { precedentJour, prochainJour, retournerJourMM } from '../code/basculerJour';
+import SingleArrow from '../media/single-arrow.svg';
+import DoubleArrow from '../media/double-arrow.svg';
 
 export default function ImgInfo({dateMM, setAjd, ajd}) {
     const idjInfo = useContext(IdjCtx);
@@ -12,10 +14,18 @@ export default function ImgInfo({dateMM, setAjd, ajd}) {
             <img className='imgFond' src={idjInfo.url}/>
             <div className="containerInfo">
                 <div className="containerBtn">
-                    <span onClick={() => retournerJourMM(dateMM, setAjd)} className='btn '>|--</span>
-                    <span onClick={() => precedentJour(-24*60*60*1000, dateMM, setAjd, ajd)} className='btn '>|-</span>
-                    <span onClick={() => prochainJour(24*60*60*1000,setAjd, ajd)} className='btn '>-|</span>
-                    <span onClick={() => setAjd(new Date())} className='btn '>--|</span>
+                    <span onClick={() => retournerJourMM(dateMM, setAjd)} className='btn left'>
+                        <img src={DoubleArrow} alt="Double arrow left" />
+                    </span>
+                    <span onClick={() => precedentJour(-24*60*60*1000, dateMM, setAjd, ajd)} className='btn left'>
+                        <img src={SingleArrow} alt="single arrow left" />
+                    </span>
+                    <span onClick={() => prochainJour(24*60*60*1000,setAjd, ajd)} className='btn '>
+                        <img src={SingleArrow} alt="single arrow right" />
+                    </span>
+                    <span onClick={() => setAjd(new Date())} className='btn '>
+                        <img src={DoubleArrow} alt="Double arrow right" />
+                    </span>
                     <p className='date'>{dateAjd(ajd)}</p>
                 </div>
                 <p className="imgDesc">{idjInfo.description ? idjInfo.description: "Aucune description"}</p>
