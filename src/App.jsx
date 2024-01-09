8083aeb8a204b92229ef2ff466f1a839152ad9f7
RoshBJr <roshanjr3@gmail.com>
2024-01-09 18:05:48 -0500

    trying to make a listed drag and drop, but can't seem to do it... yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
 import './App.css';
 import {Draggable} from './components/Draggable';
 import {Droppable} from './components/Droppable';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {DndContext} from '@dnd-kit/core';
 
 function App() {
   const [parent, setParent] = useState(null);
-  const draggable = (
-    <Draggable id="draggable">
-      <div className="container-d">
-        Go ahead, drag me.
-      </div>
-    </Draggable>
-  );
+  const [drag, setDrag] = useState([]);
+  const info = [
+    {id: 1, text: "drag me 1"},
+    {id: 2, text: "drag me 2"}
+  ]
+  const draggable = [];
+  useEffect(() => {
+    setDrag(
+      info.map( i => {
+        return(
+          <Draggable id={`draggable${i.id}`}>
+            <div className="container-d">{i.text}</div>
+          </Draggable>
+        )
+      })
+    );
+  }, [])
+
 
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      {!parent ? draggable : null}
+    <DndContext onDragMove={handleDragEnd}>
+      {/* {!parent ? draggable : null} */}
+      {/* {console.log(drag)} */}
+      {drag}
       <Droppable id="droppable">
         {parent === "droppable" ? 
-          <div className="container-drop">{draggable}</div>
+          <div className="container-drop">{drag}</div>
           : 
           <div className="container-drop">
             <h2 className="text">Drop here</h2>
@@ -30,7 +43,8 @@ function App() {
   );
 
   function handleDragEnd({over}) {
-    setParent(over ? over.id : null);
+    // setParent(over ? over.id : parent);
+    console.log(over);
   }
 }
 
